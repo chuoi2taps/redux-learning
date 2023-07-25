f2ac5f3c8941aecb89b5ce069c0ea626793bc009
chuoi2taps <thiennnph25438@fpt.edu.vn>
2023-07-25 12:31:30 +0700

    fix

--- src/context/productContext.tsx
+++ src/context/productContext.tsx
@@ -24,6 +24,7 @@ const productReducer = (state:any, action:any)=>{
         case "DELETE_PRODUCT":
             const id = action.payload.id;
             state.products = state.products.filter((item:any)=>item.id !== id)
+            return;
         default:
             return state;
     }

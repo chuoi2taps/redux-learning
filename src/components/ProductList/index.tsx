cb93daf35f11348e786f0d0e46cf94e3690b9ba9
chuoi2taps <thiennnph25438@fpt.edu.vn>
2023-07-26 09:27:39 +0700

    update assignment 1

--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -1,10 +1,57 @@
-
+import { useContext, useEffect } from "react"
+import { ProductContext } from "../../context/productContext"
+import axios from "axios"
 const ProductList = () => {
+    const {state, dispatch} = useContext(ProductContext)
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try{
+                const {data} = await axios.get("http://localhost:3000/products")
+                dispatch({type: "FETCHING", payload: data})
 
+            }
+            catch(error) {
+                console.log(error)
+            }
+        }
+        fetchProducts()
+    },[dispatch])
+
+    const addProduct = async (product:any) => {
+        try{
+            const {data} = await axios.post("http://localhost:3000/products", product)
+            dispatch({type:"ADD", payload: data})
+        }
+        catch(error) {
+            console.log(error)
+        }
+    }
+    const updateProduct = async (product:any) => {
+        try{
+            const {data} = await axios.put(`http://localhost:3000/products/${product.id}`, product)
+            dispatch({type:"UPDATE", payload: data})
+        }
+        catch(error){
+            console.log(error)
+        }
+    } 
+    const deleteProduct = async (id:number) => {
+        try{
+            await axios.delete(`http://localhost:3000/products/${id}`)
+            dispatch({type:"DELETE", payload: id})
+        }
+        catch(error){
+            console.log(error)
+        }
+    }
     return (
         <div>
-            
-
+            {state?.products?.map((item:any)=>(
+                <div key={item.id}>{item.name}</div>
+            ))}
+            <button className="bg-blue-500 rounded text-white font-bold py-2 px-4 hover:bg-blue-400" onClick={()=>addProduct({name:"product C"})}>Add</button>
+            <button className="bg-gray-500 rounded text-white font-bold py-2 px-4 mx-4 hover:bg-gray-400" onClick={()=>updateProduct({name:"product C updated", id: 3})}>Update</button>
+            <button className="bg-red-500 rounded text-white font-bold py-2 px-4 hover:bg-red-400" onClick={()=>deleteProduct(3)}>Delete</button>
         </div>
     )
 }
--- src/context/productContext.tsx
+++ src/context/productContext.tsx
@@ -1,10 +1,39 @@
+import React, {createContext, useReducer} from 'react'
+import {produce} from 'immer'
 
-const ProductProvider = () => {
-
+const initialState= {
+    products: [],
+    isLoading: false,
+    error: ""
+} as {products: any[], isLoading: boolean, error: string}
+const productReducer = (state: any, action: any) => {
+    switch(action.type) {
+        case "FETCHING":
+            state.products = action.payload
+            break;
+        case "ADD":
+            state.products.push(action.payload)
+            break;
+        case "UPDATE":
+            const product = action.payload
+            state.products = state.products.map((item:any)=>item.id === product.id ? product : item)
+            break;
+        case "DELETE":
+            const id = action.payload
+            const confirm = window.confirm("Are you sure to delete this ?")
+            if(confirm) {
+                state.products = state.products.filter((item:any)=>item.id !== id)
+            }
+            break;
+    }
+}
+export const ProductContext = createContext({} as any)
+const ProductProvider = ({children}:any) => {
+    const [state, dispatch] = useReducer(produce(productReducer), initialState)
     return (
-        <div>
-
-        </div>
+        <ProductContext.Provider value={{state, dispatch}}>
+            {children}
+        </ProductContext.Provider>
     )
 }
 
